acbe83d4541907b25caf0a14958e9ac4902a620f
Victor77 <156335309+Victor-177@users.noreply.github.com>
2026-04-24 15:46:09 +0400

    Delete app.test.js

--- app.test.js
+++
@@ -1,7 +0,0 @@
-const request = require('supertest');
-const app = require('../server'); // импортируем реальное приложение
-
-test('GET / должен вернуть 200', async () => {
-  const res = await request(app).get('/');
-  expect(res.statusCode).toBe(200);
-});
